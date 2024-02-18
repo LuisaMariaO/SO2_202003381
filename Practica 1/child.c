b7c46b0cbfdca22d4b039072d5b71bb42c952849
LUISA MARÍA ORTÍZ ROMERO <3097853160612@ingenieria.usac.edu.gt>
2024-02-18 15:28:17 -0600

    Parent con error de ctrl + c dos veces

--- Practica 1/child.c
+++ Practica 1/child.c
@@ -3,16 +3,59 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <fcntl.h>
+
+volatile sig_atomic_t sigint_received = 0; // Variable compartida para indicar si se recibió la señal SIGINT
+
+int fdch; //Para manejar practica1.txt
+
+void ctrlc_handler(int signal) {
+    sigint_received = 1; // Establecer la bandera al recibir la señal SIGINT
+    close(fdch); // Cerrar el archivo fdch
+}
+
+char random_char() {
+    const char charset[] = "0123456789"
+                           "abcdefghijklmnopqrstuvwxyz"
+                           "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const int charset_size = sizeof(charset) - 1;
+    return charset[rand() % charset_size];
+}
 
 
-int main(char *argv[]) {
-    srand(time(NULL) + getpid());
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL) + getpid()); //seed único para lograr números aleatorios
     printf("\nHola, soy tu hijo\n");
-   // while (1) {
-        // Realizar llamadas de sistema sobre "practica1.txt"
-        // Escribir, Leer, Seek
-        // Controlar intervalos de tiempo aleatorios
-   // }
+    fdch = open("practica1.txt", O_RDWR, 0777); //Abro el archivo en modo lectura y escritura
+    //Manejador de señales para SIGINT (Ctrl+C)
+    signal(SIGINT, ctrlc_handler);
+    
+  
+    while (!sigint_received) {
+        // Realizar llamadas del sistema hasta que se reciba la señal SIGINT
+        int time = rand() % 3 + 1; // Tiempo de espera entre cada operación
+        int op = rand() % 3 + 1; // Tipo de operación a realizar: 1-> write, 2-> Read, 3-> Seek
+
+        if(op==1){
+            //Write
+            char random_string[8]; // 8 caracteres alfanuméricos
+            for (int i = 0; i < 8; ++i) {
+                random_string[i] = random_char();
+            }
+            write(fdch, random_string, 8); 
+        }else if(op==2){
+            //Read
+            char buff[8];
+            read(fdch,buff,8);
+        }else{
+            //Seek
+             lseek(fdch,0,0);
+        }
+
+        sleep(time); // Esperar el tiempo especificado
+    }
 
     return 0;
 }
--- Practica 1/parent.c
+++ Practica 1/parent.c
@@ -6,61 +6,127 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <pthread.h> 
 
 int calls = 0;
 int c_write = 0;
 int c_read = 0;
 int c_seek = 0;
 
-//Para manejar la señal SIGINT (Ctrl+C)
+pthread_t monitor_thread_id;
+
+volatile sig_atomic_t sigint_received = 0; // Variable compartida para indicar si se recibió la señal SIGINT
+
 void ctrlc_handler(int signal) {
-    printf("\nFinalizando ejecución...\n");
-    printf("Llamadas al sistema de los procesos hijo: %d\n",calls);
-    printf("Llamadas write: %d\n",c_write);
-    printf("Llamadas read: %d\n",c_read);
-    printf("Llamadas seek %d\n",c_seek);
+    sigint_received = 1; // Establecer la bandera al recibir la señal SIGINT
+}
+
+//Después de recibir el ctrl+c
+void stat() {
+    printf("\n");
+    printf("Finalizando ejecución...\n");
+    printf("|-------------------------------------------------|\n");
+    printf("|Llamadas al sistema de los procesos hijo: %d      |\n",calls);
+    printf("|Llamadas write: %d                                |\n",c_write);
+    printf("|Llamadas read: %d                                 |\n",c_read);
+    printf("|Llamadas seek %d                                  |\n",c_seek);
+    printf("|-------------------------------------------------|\n");
     exit(0);
     
 }
 
-void monitor_syscalls() {
-    // Implementar la lógica para monitorear las llamadas al sistema y registrarlas en "syscalls.log"
+void monitor_syscalls(int pid1, int pid2) {
+  
+    char command[100];
+    sprintf(command, "%s %d %d %s", "sudo stap trace.stp ", pid1, pid2, " > syscalls.log 2>&1");
+    system(command);
+}
+
+void *monitor_thread(void *args) {
+    
+
+    
+    int pid1 = *((int *)args);
+    int pid2 = *((int *)args + 1);
+    monitor_syscalls(pid1, pid2);
+    pthread_exit(NULL);
+    
 }
 
 int main() {
-    //Creo el archivo si no existe, de lo contrario, se vacía
-     int fd = open("syscalls.log", O_CREAT | O_TRUNC, 0777);
-    //Manejador de señales para SIGINT (Ctrl+C)
     signal(SIGINT, ctrlc_handler);
+    //Abro el archivo syscalls.log, lo si no existe, y se existe, se vacía
+    int fd = open("syscalls.log", O_RDWR | O_CREAT | O_TRUNC, 0777);
+    //De igual manera se abre practica1.txt que será utilizado por los hijos pero se cierra acá
+    int fdch = open("practica1.txt",  O_CREAT | O_TRUNC, 0777);
+    close(fdch);
 
-   //Proceso hijo 1
-    pid_t pid1 = fork();
-    if (pid1 == 0) {
-        char *argv[] = {"/home/luisa/Escritorio/Sopes2/SO2_202003381/Practica 1/child.bin", NULL};
-        execv(argv[0], argv);
-        perror("execv"); // En caso de que execv falle
-        exit(1);
-    } else if (pid1 > 0) {
-        // Código del proceso padre
-        pid_t pid2 = fork();
-        if (pid2 == 0) {
-            // Código del segundo proceso hijo
-            char *argv[] = {"/home/luisa/Escritorio/Sopes2/SO2_202003381/Practica 1/child.bin", NULL};
-            execv(argv[0], argv);
-            perror("execv"); // En caso de que execv falle
-            exit(1);
-        } else if (pid2 < 0) {
-            perror("fork");
-            exit(1);
-        }
-    } else {
+    pid_t pid1 = fork(); //Se realiza el fork 1
+    
+    if(pid1 == -1){
         perror("fork");
         exit(1);
     }
+    if(pid1 == 0){
 
+        /*Se escriben los argumentos para el proceso hijo*/
+        char *arg_Ptr[4];
+        arg_Ptr[0] = " child.c";
+        arg_Ptr[1] = " Hola! ";
+        arg_Ptr[2] = " Soy el proceso hijo! ";
+        arg_Ptr[3] = NULL; // El ultimo indice de argv siempre debe de ser NULL
 
-    // Monitorear las llamadas al sistema de los procesos hijo
-    monitor_syscalls();
+        /*Se ejecuta el ejecutable del proceso hijo*/
+        execv("/home/luisa/Escritorio/Sopes2/SO2_202003381/Practica 1/child.bin", arg_Ptr);
+    
+     }else{
+        pid_t pid2 = fork(); //Se realiza el fork 2
+
+        if(pid2 == -1){
+            perror("fork");
+            exit(1);
+        }
+        
+        if(pid2 == 0){
+            // Código para el segundo proceso hijo
+            printf("Soy el proceso hijo 2\n");
+
+            /*Se escriben los argumentos para el proceso hijo*/
+            char *arg_Ptr[4];
+            arg_Ptr[0] = " child.c";
+            arg_Ptr[1] = " Hola! ";
+            arg_Ptr[2] = " Soy el proceso hijo! ";
+            arg_Ptr[3] = NULL; // El ultimo indice de argv siempre debe de ser NULL
+
+            /*Se ejecuta el ejecutable del proceso hijo*/
+            execv("/home/luisa/Escritorio/Sopes2/SO2_202003381/Practica 1/child.bin", arg_Ptr);
+        }else{
+            printf("Soy el proceso padre\n");
+
+            // Crear un hilo para monitorear las llamadas al sistema
+            pthread_t thread;
+            int args[2] = {pid1, pid2};
+            pthread_create(&thread, NULL, monitor_thread, (void *)args);
+
+            /*Se espera a que los procesos hijos terminen*/
+            int status;
+            waitpid(pid2, &status, 0);
+            waitpid(pid2, &status, 0);
+
+            /*Se imprime el codigo de salida de los procesos hijos*/
+            if(WIFEXITED(status)){
+
+                stat();
+            } else {
+                printf("Ocurrio un error al terminar los procesos hijos");
+            }
+            
+            // Esperar a que el hilo de monitoreo termine
+            pthread_join(thread, NULL);
+            printf("\nYa se acabó el hilo\n");
+            
+        }
+    }
 
     return 0;
 }
